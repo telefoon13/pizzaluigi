855daa3fea67f568662abceb04bf331aba8066d0
miked <mike@mikedhoore.be>
2017-08-14 10:56:17 +0200

    Hoofdstuk 8

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -1,20 +1,18 @@
 package be.vdab.servlets;
 
+import javax.servlet.http.HttpServlet;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
-public class IndexServlet extends javax.servlet.http.HttpServlet {
+public class IndexServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final String VIEW = "/WEB-INF/JSP/index.jsp";
 
+    @Override
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html lang=\"nl\"><head>");
-        out.println("<title>Pizza Luigi</title></head>");
-        out.println("<body><h1>");
         int uur = LocalDateTime.now().getHour();
-        out.println(uur >= 6 && uur < 12 ? "Goede morgen deze morgen" : uur >= 12 && uur < 18 ? "Goede middag" : "Goede avond");
-        out.println("</h1></body></html>");
+        request.setAttribute("begroeting", uur >= 6 && uur < 12 ? "Goede morgen deze morgen" : uur >= 12 && uur < 18 ? "Goede middag" : "Goede avond");
+        request.getRequestDispatcher(VIEW).forward(request, response);
     }
 }
---
+++ web/WEB-INF/JSP/index.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html>
+<html lang="nl">
+<head>
+    <title>Pizza Luigi</title>
+    <link rel="icon" href="../../images/favicon.ico">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="../../styles/default.css">
+</head>
+<body>
+<h1>pizza Luigi</h1>
+<img src="../../images/pizza.jpg" alt="Pizza" class="fullwidth">
+<h2>${begroeting}</h2>
+</body>
+</html>
